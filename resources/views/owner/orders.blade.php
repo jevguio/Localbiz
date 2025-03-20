bc1d4da4bae4d5f1d3954bf7b417910e0dd5e8a0
john-guioguio <190331326+john-guioguio@users.noreply.github.com>
2025-03-20 23:12:15 +0800

    Filter List Fixed

--- resources/views/owner/orders.blade.php
+++ resources/views/owner/orders.blade.php
@@ -55,6 +55,7 @@
                     </thead>
                     <tbody id="order-table-body">
                         @foreach ($orders as $order)
+                            @if($order->status!="on-cart")
                             <tr class="bg-white border-b border-gray-200 hover:bg-gray-50" data-category="{{ $order->status }}">
                                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                                     {{ $order->order_number }}
@@ -281,6 +282,7 @@
                                     </div>
                                 </div>
                             </div>
+                            @endif
                         @endforeach
                     </tbody>
                 </table>
