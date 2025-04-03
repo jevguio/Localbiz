a6acaa3ad1f492a9658837e559ba3ffd297a1721
john-guioguio <190331326+john-guioguio@users.noreply.github.com>
2025-04-03 23:32:35 +0800

    Profile and password Update Fixed

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -24,6 +24,7 @@ class PasswordController extends Controller
             'password' => Hash::make($validated['password']),
         ]);
 
+        session()->flash('success', 'Password Updated Successfully');
         return back()->with('status', 'password-updated');
     }
 }
