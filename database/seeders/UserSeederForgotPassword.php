9a9f2d06c723585c15881ae125bc48419a8b45a0
john-guioguio <190331326+john-guioguio@users.noreply.github.com>
2025-04-07 22:03:58 +0800

    Admin Export and Stock Fixed

---
+++ database/seeders/UserSeederForgotPassword.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeederForgotPassword extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'fname' => 'Admin',
+                'lname' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('password'),
+                'address' => 'Manila, Philippines',
+                'phone' => '09123456789',
+                'role' => 'Owner',
+                'is_active' => true,
+                'last_login' => now(),
+            ],
+
+        ];
+
+        foreach ($users as $user) {
+            $thisUser = User::where('email', '=', $user['email'])->first();
+            if ($thisUser) {
+
+                $thisUser->update([
+                    'password' => Hash::make('password')
+                ]);
+            } else {
+
+                User::create($user);
+            }
+        }
+    }
+}
