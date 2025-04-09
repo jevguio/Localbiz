37f3a11a363ff90c599507853399df21db66f598
john-guioguio <190331326+john-guioguio@users.noreply.github.com>
2025-04-09 22:36:37 +0800

    Bg added in login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,13 @@
 <x-guest-layout>
-    <div class="bg-gray-50 font-[sans-serif] p-10">
+    <div class="bg-gray-50 font-[sans-serif] p-10"
+        style="background-image: url({{ asset('assets/img/bg.jpeg') }});background-repeat: no-repeat; background-position: center ; background-size: cover; height:100vh">
         <div class=" flex flex-col items-center justify-center py-6 px-4">
             <div class="w-full flex p-10 m-10">
-                <div class="p-8 w-full  p-10 m-10" style="max-width: 50%">
-                    <div style="font-size: 6rem; font-weight: bolder; text-align: center;">Welcome To<br/> LocalBiz</div>
-                    <p style="text-align: center;">Home to Native Delicacies and <br> Souvenirs of Bohol</p>
+                <div class="p-8 w-full  p-10 m-10"
+                    style="max-width: 50%;color:white;text-shadow: -1px -1px 3px #000, 1px -1px 3px #000, -1px 1px 3px #000, 1px 1px 3px #000;">
+                    <div style="font-size: 6rem; font-weight: bolder; text-align: center;">Welcome To<br /> LocalBiz
+                    </div>
+                    <p style="text-align: center; font-size: x-large;">Support Local</p>
                 </div>
                 <div class="p-8 rounded-2xl w-full max-w-md  bg-white shadow  p-10 m-10">
                     <h2 class="text-gray-800 text-center text-2xl font-bold">Sign in</h2>
@@ -25,7 +28,7 @@
                             <div class="relative flex items-center">
                                 <input id="password" name="password" type="password" required
                                     class="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
-                                    placeholder="Enter password" id="cpass" /> 
+                                    placeholder="Enter password" id="cpass" />
                                 <i id="togglePassword" class='bx bx-show absolute right-4 cursor-pointer'></i>
                             </div>
                         </div>
@@ -46,7 +49,7 @@
         </div>
     </div>
 </x-guest-layout>
- 
+
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
     $(document).ready(function() {
