ec0a1007d4f36347fb39025de9fd6c3a56a6ffa7
saggy-alt <gmaevertulfo@gmail.com>
2025-04-11 04:46:40 +0800

    sign in bg

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,33 +1,33 @@
 <x-guest-layout>
-    <div class="bg-gray-50 font-[sans-serif] p-10"
+    <div class="bg-gray-50 font-[sans-serif]"
         style="background-image: url({{ asset('assets/img/background.png') }});background-repeat: no-repeat; background-position: center ; background-size: cover; height:100vh">
         <div class=" flex flex-col items-center justify-center py-6 px-4">
             <div class="w-full flex p-10 m-10 ">
                 <div class="p-8 w-full  p-10 m-10 "
-                    style="max-width: 50%;color:white;text-shadow: -1px -1px 3px #000, 1px -1px 3px #000, -1px 1px 3px #000, 1px 1px 3px #000; ">
+                    style="max-width: 50%;color:white;">
                     <div style="font-size: 6rem; font-weight: bolder; text-align: center;line-height: 1;margin: 0;">Welcome To<br /> LocalBiz
                     </div>
                     <p style="text-align: center; font-size: x-large;">Support Local</p>
                 </div>
-                <div class="p-8 rounded-2xl w-full max-w-md  shadow  p-10 m-10 " style="background-color: rgba(158, 158, 158, 0.616)">
-                    <h2 class="text-gray-800 text-center text-2xl font-bold">Sign in</h2>
+                <div class="p-8 rounded-2xl w-full max-w-md  shadow  p-10 m-10 " style="background-color: rgba(20, 20, 20, 0.85)">
+                    <h2 class="text-white text-center text-2xl font-bold">Sign In</h2>
                     <form class="mt-8 space-y-4" method="POST" action="{{ route('login') }}">
                         @csrf
                         <div>
-                            <label class="text-gray-800 text-sm mb-2 block">Email Address</label>
+                            <label class="text-white text-base mb-2 block">Email Address</label>
                             <div class="relative flex items-center">
                                 <input id="email" name="email" type="email" required
-                                    class="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
+                                    class="w-full text-white text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
                                     placeholder="Enter email address" />
                                 <i class='bx bxs-user absolute right-4'></i>
                             </div>
                         </div>
 
                         <div>
-                            <label class="text-gray-800 text-sm mb-2 block">Password</label>
+                            <label class="text-white text-base mb-2 block">Password</label>
                             <div class="relative flex items-center">
                                 <input id="password" name="password" type="password" required
-                                    class="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
+                                    class="w-full text-white text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
                                     placeholder="Enter password" id="cpass" />
                                 <i id="togglePassword" class='bx bx-show absolute right-4 cursor-pointer'></i>
                             </div>
@@ -35,11 +35,11 @@
 
                         <div class="!mt-8">
                             <button type="submit"
-                                class="w-full py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none">
+                                class="w-full py-3 px-4 text-base tracking-wide rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none">
                                 Sign in
                             </button>
                         </div>
-                        <p class="text-gray-800 text-sm !mt-8 text-center">Don't have an account? <a
+                        <p class="text-white text-sm !mt-8 text-center">Don't have an account? <a
                                 onclick="window.location.href='{{ route('register') }}'"
                                 class="text-blue-600 hover:underline ml-1 whitespace-nowrap font-semibold">Register
                                 here</a></p>
