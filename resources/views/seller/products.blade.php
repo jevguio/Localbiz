49f7810a19c693a3f95321432436cecac4cd12d3
John Vincent S. Guioguio <125118778+jevguio@users.noreply.github.com>
2025-04-23 13:17:47 +0800

    stock edit update

--- resources/views/seller/products.blade.php
+++ resources/views/seller/products.blade.php
@@ -139,7 +139,7 @@
                                                     <div class="col-span-2 sm:col-span-1">
                                                         <label for="stock"
                                                             class="block mb-2 text-sm font-medium text-gray-900">Stock</label>
-                                                        <input type="number" name="stock" id="stock"
+                                                        <input type="number" name="stock" id="stock" readonly disabled
                                                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                                             value="{{ old('stock', $product->stock) }}">
                                                     </div>
