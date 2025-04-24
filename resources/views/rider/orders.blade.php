61a8fb0bbb07994174cd98bc85c224f16795fcdf
saggy-alt <gmaevertulfo@gmail.com>
2025-04-25 02:47:18 +0800

    total amount renamed

--- resources/views/rider/orders.blade.php
+++ resources/views/rider/orders.blade.php
@@ -155,7 +155,7 @@
                                                 </div>
                                                 <div class="col-span-1">
                                                     <label for="price"
-                                                        class="block mb-2 text-sm font-medium text-gray-900">Total Amoount</label>
+                                                        class="block mb-2 text-sm font-medium text-gray-900">Total Amount</label>
                                                     <input type="number" name="price" id="price"
                                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                                         placeholder="$2999"
