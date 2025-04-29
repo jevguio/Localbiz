dc78d16ba8dcf108ff0554f3102884f2de88b152
saggy-alt <gmaevertulfo@gmail.com>
2025-04-29 19:13:16 +0800

    nav bar

--- resources/views/rider/orders.blade.php
+++ resources/views/rider/orders.blade.php
@@ -7,20 +7,20 @@
             <div class="relative overflow-x-auto mt-10 bg-white p-4 rounded-lg">
                 <form class="max-w-md ml-0 mb-4">
                     <label for="table-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
-                    <div class="relative">
+                    <!-- <div class="relative">
                         <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                             <i class='bx bx-search text-gray-500 text-2xl'></i>
-                        </div>
-                        <input type="search" id="table-search"
+                        </div> -->
+                        <!-- <input type="search" id="table-search"
                             class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-                            placeholder="Search for products....">
+                            placeholder="Search for products...."> -->
                             
-                        <button type="button" id="filter-btn"
+                        <!-- <button type="button" id="filter-btn"
                             class="absolute inset-y-0 end-0 flex items-center px-3 text-gray-600 hover:text-gray-900">
                             <i class='bx bx-filter text-2xl'></i>
-                        </button>
+                        </button> -->
                         
-                        {{-- <div id="filter-dropdown"
+                        <!-- {{-- <div id="filter-dropdown"
                             class="hidden absolute right-0 mt-2 w-40 bg-white border border-gray-300 rounded-lg shadow-lg">
                             <ul class="py-2 text-sm text-gray-700">
                                 <li class="px-4 py-2 hover:bg-gray-100 cursor-pointer filter-option" data-filter="All">All</li>
@@ -30,7 +30,7 @@
                                 <li class="px-4 py-2 hover:bg-gray-100 cursor-pointer filter-option" data-filter="delivered">Delivered</li> 
                                 <li class="px-4 py-2 hover:bg-gray-100 cursor-pointer filter-option" data-filter="cancelled">Cancelled</li> 
                             </ul>
-                        </div> --}}
+                        </div> --}} -->
                     </div>
                 </form>
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500">
