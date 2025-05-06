9ef2d578beab49d563fa61b489d9841dd6602f8f
saggy-alt <gmaevertulfo@gmail.com>
2025-05-07 00:41:05 +0800

    spacing

--- resources/views/rider/orders.blade.php
+++ resources/views/rider/orders.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="p-4 sm:ml-64">
         <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg">
-            <div class="grid grid-cols-3 items-center gap-4">
+            <div class="grid grid-cols-3 items-center ml-3 gap-4">
                 <h2 class="text-xl font-bold text-gray-900 sm:text-2xl">Order Management</h2>
             </div>
 
@@ -102,7 +102,7 @@
                                         <div
                                             class="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-200">
                                             <h3 class="text-lg font-bold text-gray-900">
-                                                Order Details
+                                                 Order Details
                                             </h3>
                                             <button type="button"
                                                 class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
@@ -287,10 +287,6 @@
                                                 </div>
                                                 <hr class="my-4">
                                                 <div class="flex justify-end gap-2">
-                                                    <button type="submit" name="payment_status" value="fully_paid"
-                                                        class="btn btn-success bg-green-700 hover:bg-green-800 text-white inline-flex items-center focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                                                        Fully Paid
-                                                    </button>
                                                     <button type="submit"
                                                         class="btn btn-prumary bg-blue-700 hover:bg-blue-800 text-white inline-flex items-center focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                                                         Update
