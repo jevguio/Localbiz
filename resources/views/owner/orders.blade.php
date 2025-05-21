c72058e76f7bf9dfcf23259b34706665210de3e0
John Vincent S. Guioguio <125118778+jevguio@users.noreply.github.com>
2025-05-21 11:34:27 +0800

    order owner fixed

--- resources/views/owner/orders.blade.php
+++ resources/views/owner/orders.blade.php
@@ -250,7 +250,7 @@
                                             <div name="status" id="status"
                                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
 
-                                            @if(optional($item->order->payments)->delivery_method == 'cod')
+                                            @if(optional($order->payments)->delivery_method == 'cod')
                                             Cash on Delivery
                                             @else
                                             Pick Up
